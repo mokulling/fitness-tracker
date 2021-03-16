0e634f3252388fca86295f7054b435618cfc52b0
mokulling <72229978+mokulling@users.noreply.github.com>
2021-03-15 19:24:08 -0500

    rendered exercise page

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const mongojs = require("mongojs");
 const logger = require("morgan");
 const mongoose= require('mongoose')
+const path = require('path')
 
-const databaseUrl = "workout";
-const collections = ["exercises"];
-const db = mongojs(databaseUrl, collections);
+// const databaseUrl = "Workout";
+// const collections = ["exercises"];
+// const db = mongojs(databaseUrl, collections);
+const db = require('./models')
+const Workout = require('./models/Workout')
 
 const app = express();
 
@@ -17,11 +20,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+  
+
+app.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+
+})
+
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'exercise.html'));
+
+})
+
 
 
 
